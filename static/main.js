ac846f31e7fb7f87a1d729c4eabd1c4e55b673a7
Cyber-Kaeh <153845457+Cyber-Kaeh@users.noreply.github.com>
2025-01-09 01:57:44 -0500

    Implemented Bootstrap accordion to accomodate more APIs. App now has famous quotes and the JFT.

--- static/main.js
+++ static/main.js
@@ -1,12 +1,13 @@
 import { MY_API_KEY } from "./config.js";
 
 function onLoad() {
-    getJoke();
+    getChuckJoke();
 }
 
-document.getElementById('get-jokes').addEventListener('click', getJoke);
+document.getElementById('get-chuck-jokes').addEventListener('click', getChuckJoke);
+document.getElementById('get-quote').addEventListener('click', getQuote);
 
-async function getJoke() {
+async function getChuckJoke() {
     const url = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random';
     const options = {
         method: 'GET',
@@ -28,4 +29,29 @@ async function getJoke() {
     }
 }
 
-getJoke();
+async function getQuote() {
+    const url = 'https://quotes15.p.rapidapi.com/quotes/random/?language_code=en';
+    const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '2751fda6c9msh7b5342f8df30de4p18f4abjsn89107bfbc39d',
+		'x-rapidapi-host': 'quotes15.p.rapidapi.com'
+	}
+};
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const name = result.originator.name;
+        const content = result.content;
+        const desc = result.originator.description;
+        document.getElementById('quote').innerHTML = `<p>Name: ${name}<br>Quote: ${content}`;
+        console.log(desc);
+        console.log()
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getChuckJoke();
+getQuote();
